e8f6575c93fd266607d42b1133e2fd83350050d3
Harsh <harsh.srivastava51@gmail.com>
2024-08-12 12:51:24 +0200

    --removed unnecessary validation

--- src/stock/repository/stock.repository.ts
+++ src/stock/repository/stock.repository.ts
@@ -9,9 +9,6 @@ import { MongoRepository, Document, DataSource } from "typeorm";
 import { AppLogger } from "src/core/logger";
 import { ObjectLiteral } from "src/common/interfaces/object-literal.interface";
 import { UpdateResult } from "mongodb";
-import { NotFoundException } from "src/common/exceptions/not-found.exception";
-import { HttpResponseMessage } from "src/common/enums/response-message.enum";
-import { HttpStatusCodes } from "src/common/enums/http-status-codes.enum";
 
 @Injectable()
 export class ProductStockRepository implements IStockRepository {
@@ -49,16 +46,6 @@ export class ProductStockRepository implements IStockRepository {
 
     try {
       const product = await this.findOne(query);
-
-      if (!product) {
-        throw new NotFoundException({
-          message: HttpResponseMessage.NOT_FOUND,
-          httpStatusCode: HttpStatusCodes.FAILED,
-          details: {
-            message: `stock with productId ${query.productId} and warehouseId: ${query.warehouseId} does not exist`,
-          },
-        });
-      }
       newAvailability = product.availability - productCount;
       await this.updateOne(query, {
         $set: {
