f2abcdacc1fdd21924f52c952624e23dc129dc87
Harsh <harsh.srivastava51@gmail.com>
2024-08-12 18:56:18 +0200

    --moved kafka module to infra layer

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -2,8 +2,8 @@ import { Module } from "@nestjs/common";
 import { HttpModule } from "src/infra/http/http.module";
 import { HttpService } from "src/infra/http/http.service";
 import { NotificationService } from "./notification.service";
-import { KafkaModule } from "src/kafka/kafka.module";
-import { ProducerService } from "src/kafka/producer.service";
+import { KafkaModule } from "src/infra/kafka/kafka.module";
+import { ProducerService } from "src/infra/kafka/producer.service";
 
 @Module({
   imports: [KafkaModule, HttpModule],
